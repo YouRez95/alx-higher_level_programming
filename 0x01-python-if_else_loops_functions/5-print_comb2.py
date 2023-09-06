5eaf7374ca7ac7742fb69149d73270b0a05e1651
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-06 01:42:19 +0100

    print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i > 9 and i < 99:
-        print("{}, ".format(i), end="")
+        print("0{},".format(i), end=" ")
     else:
-        print("{}".format(i))
+        print("{},".format(i), end=" ")
