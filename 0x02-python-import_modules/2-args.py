98adb6f27b73473ded69417cd7201e986f6c7dbf
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-07 15:09:58 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} {}:".format(
+                              len(sys.argv) - 1,
+                              "arguments" if len(sys.argv) > 2
+                              else "argument"))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
