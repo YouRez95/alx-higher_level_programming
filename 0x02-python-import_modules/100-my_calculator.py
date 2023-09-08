1fcad1fcba757c2c39acf22eb73f5ba559c6b3eb
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-08 01:53:35 +0100

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,6 @@ import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print(sys.argv)
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
@@ -18,6 +17,8 @@ if __name__ == '__main__':
         print("{} * {} = {}".format(a, b, mul(a, b)))
         exit(0)
     if sys.argv[2] == '/':
+        if b == 0:
+            exit(1)
         print("{} / {} = {}".format(a, b, div(a, b)))
         exit(0)
     else:
