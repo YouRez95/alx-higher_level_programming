a4bbca4d87bd51d84d8e2a5465a93994e153b5db
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-11 21:50:18 +0100

    print_matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        pass
+    if matrix[-1] == []:
+        print("")
     else:
         for row in matrix:
             for i in range(len(row)):
