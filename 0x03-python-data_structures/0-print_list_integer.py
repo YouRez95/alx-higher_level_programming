41257145430a9eba238b931b57e9d8ef06ef9446
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-11 19:45:45 +0100

    print_list_of_integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print('{}'.format(integer))
+        for index in range(len(my_list)):
+            print('{}'.format(my_list[index]))
