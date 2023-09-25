db48a58565152d703f89bc0b591c5a54280736d6
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-25 20:57:43 +0100

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as er:
-        sys.stderr.write("Exception: {}".format(er))
+        sys.stderr.write("Exception: {}\n".format(er))
         return False
