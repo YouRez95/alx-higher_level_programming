9991689a2ed3bd4dd90787409519e63f2d06d2c8
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-25 19:42:20 +0100

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         print("", end="\n")
         return count
-    except Exception as ex:
-        print(ex.args)
+    except Exception:
+        raise
