ce1560584e4b38a4e1ebc61ac9eb47c61e5d1ac8
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-25 18:03:21 +0100

    safe_list_printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            end = "\n" if i == x - 1 else ""
+            print(my_list[i],  end=end)
+        except IndexError:
+            print("", end="\n")
+            return i
+    return x
