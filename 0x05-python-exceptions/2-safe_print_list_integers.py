17f16843b3d32440f7fa6f7bd8fa4fa1955d8931
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-25 19:38:00 +0100

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{}".format(my_list[i]), end="")
+                count += 1
+        print("", end="\n")
+        return count
+    except Exception as ex:
+        print(ex.args)
