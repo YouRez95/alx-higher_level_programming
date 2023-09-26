30a149dace4ab8de7bc24d43f48e5fd4bcc3ca3a
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-26 20:20:03 +0100

    access_update_private_value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+""" Module
+This is a module for creating a new class
+Classes:
+    Square: class represent a square
+"""
+
+
+class Square:
+    """ class representing a square
+    This class alows us to create and manipulate square
+    Attributes:
+        size: size of square
+    """
+    def __init__(self, size=0):
+        """ Initialize new instances of square
+
+        args:
+            size: size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """  get the size of square
+         Returns:
+             size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """  set the size of square
+             args:
+                 value: size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """  Caluculate the area of square
+         Returns:
+             the area
+        """
+        return self.__size * self.__size
