27b2362364f133839501e95b1f5fa0a185a071ab
Youness Elalouani <younesselalouani1995@gmail.com>
2023-09-26 12:42:40 +0100

    first_class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+""" class representing a square
+    This class alows us to create and manipulate square
+    """
 class Square:
     """ class representing a square
     This class alows us to create and manipulate square
