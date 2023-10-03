e60854aaa95c2b3239e054eb9845a9be3c4e620e
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-03 12:37:40 +0100

    line_exec

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string._s = getattr(magic_string, "_s", "BestSchool") + "BestSchool"
+    magic_string._s = getattr(magic_string, "_s", "BestSchool,") + "BestSchool"
     return magic_string._s
