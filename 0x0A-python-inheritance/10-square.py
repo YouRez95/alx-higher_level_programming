6d7a0391d3f6630179356635f02b0cbe4c9e50c4
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-09 22:36:48 +0100

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+""" Module """
+
+
+class Square(Rectangle):
+    """ class """
+    def __init__(self, size):
+        """ init """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ area """
+        return self.__size * self.__size
