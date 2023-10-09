3a76b7ca9add6705ba2c01fc8cff2f2ab81c9b8d
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-09 22:27:40 +0100

    rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class BaseGeometry:
+    """ my Class """
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ integer validator """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ class """
+    def __init__(self, width, height):
+        """ init """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ return area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ print """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
