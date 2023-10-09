358373744864836c12a9ec062dcc95e69d377b16
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-09 16:53:18 +0100

    is_kind_od_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" My Module """
+
+
+def is_kind_of_class(obj, a_class):
+    """ is inherit from class """
+    if isinstance(obj, a_class):
+        return True
+    return False
