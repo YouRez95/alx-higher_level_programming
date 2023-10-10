c1bfef36582d3306dd3c6087d8b314bf6de75a5b
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-10 19:20:12 +0100

    get_attr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module """
+
+
+class Student:
+    """ class Student """
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json """
+        my_dict = {}
+        if attrs is not None:
+            for attr in attrs:
+                if hasattr(self, attr):
+                    my_dict[attr] = getattr(self, attr)
+            return my_dict
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
