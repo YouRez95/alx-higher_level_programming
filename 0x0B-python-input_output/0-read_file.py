56a0e16813a6d75a5e9f7e0d2c6a20359c7b7833
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-10 11:11:41 +0100

    read-file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module """
+
+
+def read_file(filename=""):
+    """ function for read file """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
