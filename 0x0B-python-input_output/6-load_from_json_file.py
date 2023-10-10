247ead5fc4a7f776e5b8bd58eafdc29d08c238bd
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-10 12:25:15 +0100

    json_to_ds

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module """
+
+
+from json import load
+
+def load_from_json_file(filename):
+    """ function that deserialize data from json file """
+    with open(filename, "r") as json_file:
+        data = load(json_file)
+        
+    return data
