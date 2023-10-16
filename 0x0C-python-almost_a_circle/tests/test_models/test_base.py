53f9c30ae1615a35e00e04c0c078c65e8f4e3ba0
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-16 23:02:55 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,16 +7,30 @@ from models.square import Square
 
 class TestBase(unittest.TestCase):
     """ test base class """
+
+    def setUp(self):
+        """ setup """
+        Base._nb_objects = 0
+
     def test_id(self):
+        """ test id """
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
     def test_init(self):
+        """ test init """
         with self.assertRaises(TypeError) as err:
             Base.__init__()
         err_msg = "Base.__init__() missing 1 required positional argument: 'self'"
         self.assertEqual(err_msg, str(err.exception)) 
 
+    def test_more_id(self):
+        """ id """
+        b = Base()
+        b = Base()
+        b = Base(12)
+        b = Base()
+        self.assertEqual(getattr(Base, "_nb_objects"), b.id)
 
 if __name__ == '__main__':
     unittest.main()
