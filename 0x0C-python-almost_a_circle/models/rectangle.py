3b29f2ecb8bb007b919a8cec8e457a90f47eaea9
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-16 19:27:40 +0100

    update_rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,8 +82,10 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ update attr """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
         i = 1
         for arg in args:
             if i == 1:
