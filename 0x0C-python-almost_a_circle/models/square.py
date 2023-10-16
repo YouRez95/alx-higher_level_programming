471b2b353b4639b5853afe533d13867ef743227a
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-16 19:48:16 +0100

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ constructor """
+        super().__init__(size, size, x, y)
+
+    def __str__(self):
+        """ __str__ """
+        return "[Square] ({}) {}/{} - {}".format(
+               self.id, self.x, self.y, self.width)
