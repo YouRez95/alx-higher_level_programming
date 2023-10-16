f96728ca90dbe3623d09015098d9df33281ad69a
Youness Elalouani <younesselalouani1995@gmail.com>
2023-10-16 19:57:17 +0100

    square__getter_setter\

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,14 @@ class Square(Rectangle):
         """ __str__ """
         return "[Square] ({}) {}/{} - {}".format(
                self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """ get size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ set size """
+        self.width = value
+        self.height = value
