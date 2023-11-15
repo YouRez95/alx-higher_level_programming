783c3a84ba038bf65c119efce0a048c4b7b6ee10
Youness Elalouani <younesselalouani1995@gmail.com>
2023-11-15 02:46:29 +0100

    list_records_desc

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- list all records of tbale
 -- result should display the score and the name in this order
 -- in descending order
-SELECT score, name FROM second_table ORDER BY score DESC; 
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC; 
