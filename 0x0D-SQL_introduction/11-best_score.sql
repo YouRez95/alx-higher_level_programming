fa27e640456592b39eaba798af7d5b713c2f0acd
Youness Elalouani <younesselalouani1995@gmail.com>
2023-11-15 02:01:52 +0100

    select_the_best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- list all records
+-- display only score and name in this order
+-- records should be ordered by score where score >=10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
