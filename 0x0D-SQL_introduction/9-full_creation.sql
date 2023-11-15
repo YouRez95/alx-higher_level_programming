1a80d35b3a9fb687421f9935c7ade781abdbc7fb
Youness Elalouani <younesselalouani1995@gmail.com>
2023-11-15 01:46:27 +0100

    add_multiple_records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- create a table in the database
+-- in that tables create multiple records
+CREATE TABLE IF NOT EXISTS second_table (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), score INT);
+INSERT INTO second_table(name,score) VALUES('John',10),('Alex',3),('Bob',14),('George',8);
