94f0d4df3f5505da7c6fc2cdb314033d35ce547b
Youness Elalouani <younesselalouani1995@gmail.com>
2023-11-16 01:05:23 +0100

    root_user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create a new user
+-- and give the user all permissions
+-- if the user already exist the script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
