7ef4fcd2278f237e8ed54c94de615e2ae37e9d53
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-11 17:42:46 +0100

    value arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 const process = require('process');
 
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
