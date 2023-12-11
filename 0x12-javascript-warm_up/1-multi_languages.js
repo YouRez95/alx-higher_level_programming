cf7fcb95d6b31355120789810c445022e984bf68
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-11 16:51:31 +0100

    console three times

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
 
-const firstLine = 'C is fun';
-const secondLine = 'Python is cool';
-const thirdLine = 'JavaScript is amazing';
-console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
+console.log('C is fun' + '\n' + 'Python is cool' + '\n' + 'JavaScript is amazing');
