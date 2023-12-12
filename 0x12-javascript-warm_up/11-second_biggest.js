c86b74339f3408ca270418a64f6ec685f71e7224
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-12 09:27:03 +0100

    find the second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+function secondBiggest (numbers) {
+  let bigg = numbers[2] > numbers[3] ? numbers[2] : numbers[3];
+  let secBigg = numbers[2] > numbers[3] ? numbers[3] : numbers[2];
+  let pointer = 4;
+  while (numbers[pointer]) {
+    if (parseInt(numbers[pointer]) > parseInt(bigg)) {
+      secBigg = bigg;
+      bigg = numbers[pointer];
+    }
+    pointer++;
+  }
+
+  return parseInt(secBigg);
+}
+
+if (argv.length === 2 || argv.length === 3) {
+  console.log(0);
+} else {
+  console.log(secondBiggest(argv));
+}
