86179f947ee413ba8f25dc9fff4161258ea149da
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-14 23:20:20 +0100

    select states by name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""select states with a name starting with N"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         password=sys.argv[2], database=sys.argv[3])
+    sql = db.cursor()
+    sql.execute(
+        "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id")
+
+    result = sql.fetchall()
+
+    for r in result:
+        print(r)
+    sql.close()
