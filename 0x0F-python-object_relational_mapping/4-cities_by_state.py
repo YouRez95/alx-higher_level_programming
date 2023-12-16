8c475c9a86d88cb274102609c4d54f0c355bb0f5
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-16 16:05:20 +0100

    list all cities with states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,9 +11,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                          password=sys.argv[2], database=sys.argv[3])
     sql = db.cursor()
-    query = "SELECT * FROM cities ORDER BY cities.id"
+    query = """SELECT c.id, c.name, s.name
+            FROM cities c
+            JOIN states s ON c.state_id = s.id
+            ORDER BY c.id"""
     sql.execute(query)
     result = sql.fetchall()
-
+    # print(type(result))
+    # print(result)
     for r in result:
         print(r)
