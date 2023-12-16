0acfb5391daa0b4035e76fe7ae9ef88a0af89fbd
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-17 00:00:51 +0100

    relationship states cities

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -17,8 +17,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id)
-    print(result)
-    # print("{}: {}".format(s.id, s.name))
-    # for c in s.cities:
-    #     print("     {}: {}".format(c.id, c.name))
+    for s in session.query(State).order_by(State.id):
+        print("{}: {}".format(s.id, s.name))
+        for c in s.cities:
+            print("     {}: {}".format(c.id, c.name))
