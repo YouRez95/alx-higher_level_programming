9953a41a969d4efcd47e247622a52dc09b12674d
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-16 18:40:02 +0100

    limit the query

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+  fectch all state ordered by id and limitid by 1 row
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    results = session.query(State).order_by(State.id)
+    results = session.query(State).limit(1)
+    # print(results)
+    if not results:
+        print("Nothing")
+    for r in results:
+        print(r)
