00704fdcae0c64e60f82c83e8bc1c13282f21f97
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-05 15:56:29 +0100

    post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send Post request with body
-curl -s -X POST -d '{"email":"test@gmail.com", "subject": "I will always be here for PLD"}' $1
+curl -s -X POST -d 'email:test@gmail.com&subject: I will always be here for PLD' $1
