7f23388a584268f6b7a3c5706a9fe5a39200ccbd
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-06 22:16:21 +0100

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+  module to send a request and get response
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        owner_name, repo_name)
+    headers = {'Accept': 'application/vnd.github+json',
+               'X-GitHub-Api-Version': '2022-11-28'}
+    response = requests.get(url, headers=headers)
+    response = response.json()
+    for i in range(10):
+        print("{}: {}".format(response[i]['sha'],
+              response[i]['commit']['author']['name']))
