b3dc8b5beaeb1db7ef8253a56e784c9134f53672
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-06 15:14:10 +0100

    get header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-import urllib.request
 """
   module to send a request and get X-Request-Id header
 """
+
+import sys
+import urllib.request
 
 
 if __name__ == "__main__":
