655866548c4fe3663f38487faa106d1c50db7391
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-06 14:23:39 +0100

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print('Body response:')
-    print('      - type: {}'.format(type(html)))  # class bytes
-    print('      - content: {}'.format(html))  # b'OK'
-    print('      - utf8 content: {}'.format(response.msg))  # OK
+    print('    - type: {}'.format(type(html)))  # class bytes
+    print('    - content: {}'.format(html))  # b'OK'
+    print('    - utf8 content: {}'.format(response.msg))  # OK
