02ef5cfc25657eca195bb441f02d027fb9c863b8
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-06 21:04:49 +0100

    read id of user by using token

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+  module to send a request and get response
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    headers = {'Authorization': 'Bearer {}'.format(
+        token), 'X-GitHub-Api-Version': '2022-11-28'}
+    url = "https://api.github.com/users/{}".format(username)
+    response = requests.get(url, headers=headers)
+    print(response.headers.get('X-GitHub-Request-Id'))
