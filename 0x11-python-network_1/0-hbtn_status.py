47faf61edd317276fb5cfe061e28fa797e8df618
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-06 14:50:20 +0100

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ from urllib import request
 
 with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     peek = response.peek()
-    html = response.read()
-    utf_content = html.decode('UTF-8')
+    data = response.read()
+    utf_content = data.decode('UTF-8')
     print('Body response:')
-    print('    - type: {}'.format(type(html)))
+    print('    - type: {}'.format(type(data)))
     print('    - content: {}'.format(peek))
     print('    - utf8 content: {}'.format(utf_content))
