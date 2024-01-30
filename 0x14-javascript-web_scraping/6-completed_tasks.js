5cae061a06f66ed81b200cbac50b77aba8ee9b85
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-30 12:00:56 +0100

    fix the completed task file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,12 +10,14 @@ request(url, (err, res, body) => {
   if (err) console.log(err);
   else {
     const response = JSON.parse(body);
-    const completedTask = {};
-    for (const user of response) {
-      const userId = user.userId;
-      if (!(userId in completedTask)) completedTask[userId] = 0;
-      if (user.completed === true) completedTask[userId] += 1;
+    if (response.length > 0) {
+      const completedTask = {};
+      for (const user of response) {
+        const userId = user.userId;
+        if (!(userId in completedTask)) completedTask[userId] = 0;
+        if (user.completed === true) completedTask[userId] += 1;
+      }
+      console.log(completedTask);
     }
-    console.log(completedTask);
   }
 });
