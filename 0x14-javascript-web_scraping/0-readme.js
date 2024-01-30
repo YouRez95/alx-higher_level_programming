bdf583bb50ea33077aaee9420fa5b98026ae2c36
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-30 10:13:53 +0100

    add file that can make request and get the code status

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,7 +8,5 @@ const nameOfFile = process.argv[2];
 
 fs.readFile(nameOfFile, { encoding: 'utf8' }, (err, data) => {
   if (err) console.log(err);
-  else {
-    console.log(data);
-  }
+  else console.log(data);
 });
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * make request and get the status code
+ */
+
+const request = require('request');
+
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  else console.log('code: ' + res.statusCode);
+});
