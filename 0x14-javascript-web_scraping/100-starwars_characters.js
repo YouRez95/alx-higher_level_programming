789c8467cb477f0258ae6cc1ef2b17aeadbe2094
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-30 12:51:44 +0100

    add file that can make request and print all characters in order using promise

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,31 @@
 #!/usr/bin/node
 /**
- * make request and prints all characters of a Star Wars movie
+ * make request and prints all characters of a Star Wars movie in order
  */
 
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, (err, res, body) => {
-  if (err) console.log(err);
-  else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, (err, res, body) => {
-        if (err) console.log(err);
-        else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const urlFilms = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+function makeRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) reject(err);
+      else resolve(JSON.parse(body));
+    });
+  });
+}
+
+async function printCharacter () {
+  try {
+    const responseMovies = await makeRequest(urlFilms);
+    for (const character of responseMovies.characters) {
+      const responseCharacter = await makeRequest(character);
+      console.log(responseCharacter.name);
     }
+  } catch (err) {
+    console.log(err);
   }
-});
+}
+
+printCharacter();
