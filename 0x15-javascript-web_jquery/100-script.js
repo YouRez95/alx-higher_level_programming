dc949d1445a74fd758419d3aacc3fba442ce6df1
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-07 13:16:24 +0100

    change the color of header by using attribute defer

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  const script = document.querySelector('script');
+  script.setAttribute('defer', 'defer');
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
